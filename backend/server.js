82dddddc94fff63c9e9e0c7a6e44c1c88b124c2c
Umar Farooq <149324609+umarfarooq6519@users.noreply.github.com>
2024-11-10 14:56:04 +0500

    ready for prod

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
 
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
@@ -9,9 +10,19 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // allows us to use JSON data in req.body
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
