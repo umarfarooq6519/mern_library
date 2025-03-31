df549f435030e459d76b2ca37fed69ca9af70210
Umar Farooq <149324609+umarfarooq6519@users.noreply.github.com>
2025-04-01 01:00:09 +0500

    seperate backend and frontend logic

--- backend/server.js
+++ backend/server.js
@@ -16,13 +16,13 @@ app.use(express.json()); // allows us to use JSON data in req.body
 
 app.use("/api/products", productRoutes);
 
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// if (process.env.NODE_ENV == "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+//   });
+// }
 
 app.listen(PORT, () => {
   connectDB();
