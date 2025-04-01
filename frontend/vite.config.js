ae4b845a5847c7fb0e95071d7ecf0e412186b04f
Umar Farooq <149324609+umarfarooq6519@users.noreply.github.com>
2025-04-01 11:36:33 +0500

    minor changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://mern-product-app-server.onrender.com",
-        changeOrigin: true,
-        secure: false, // <- Disable SSL verification
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "https://mern-product-app-server.onrender.com",
+  //       changeOrigin: true,
+  //       secure: true,
+  //     },
+  //   },
+  // },
 });
